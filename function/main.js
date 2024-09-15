eff377a979f198c8c65b6e3065ee66a9b1db17ee
viktorKorneev <korneevviktor90@yandex.ru>
2024-09-15 22:53:16 +0300

    step-3

--- function/main.js
+++ function/main.js
@@ -1,24 +1,49 @@
-let migrating = true;
+// let migrating = true;
 
-      let fly = function (num) {
-        for (let i = 0; i < num.length; i++) {
-          alert("Flying!");
-        }
-      };
+//       let fly = function (num) {
+//         for (let i = 0; i < num.length; i++) {
+//           alert("Flying!");
+//         }
+//       };
 
-      function quack(num) {
-        for (let i = 0; i < num.length; i++) {
-          alert("Quack!");
-        }
-      }
+//       function quack(num) {
+//         for (let i = 0; i < num.length; i++) {
+//           alert("Quack!");
+//         }
+//       }
 
-      let superFly = fly;
-      superFly(2);
+//       let superFly = fly;
+//       superFly(2);
 
-      let superQuack = quack;
-      superQuack(3);
+//       let superQuack = quack;
+//       superQuack(3);
 
-      // if (migrating) {
-      //   quack(4);
-      //   fly(4);
-      // }
+// if (migrating) {
+//   quack(4);
+//   fly(4);
+// }
+
+let winner = function () {
+  console.log("WINNER!");
+  
+};
+let loser = function () {
+  console.log("LOSER!");
+};
+
+winner();
+
+let a = winner;
+let b = loser;
+let c = loser;
+a();
+b();
+
+c = a;
+a = b;
+b = c;
+c = a;
+a = c;
+a = b;
+b = c;
+a();
