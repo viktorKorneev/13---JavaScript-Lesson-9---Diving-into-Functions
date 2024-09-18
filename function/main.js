f51bd5fe3218da13edb4d853abd6c7c18205c6a9
viktorKorneev <korneevviktor90@yandex.ru>
2024-09-18 22:23:07 +0300

    step-14

--- function/main.js
+++ function/main.js
@@ -49,77 +49,93 @@
 // a();
 // ----------------------------------
 
-let passengers = [
-  { name: "Jane Doloop", paid: true, ticket: "coach" },
-  { name: "Dr. Evel", paid: true, ticket: "firstclass" },
-  { name: "Sue Property", paid: false, ticket: "firstclass" },
-  { name: "John Funcall", paid: true, ticket: "coach" },
-];
+// let passengers = [
+//   { name: "Jane Doloop", paid: true, ticket: "coach" },
+//   { name: "Dr. Evel", paid: true, ticket: "firstclass" },
+//   { name: "Sue Property", paid: false, ticket: "firstclass" },
+//   { name: "John Funcall", paid: true, ticket: "coach" },
+// ];
 
-function processPassengers(passengers, testFunction) {
-  for (let i = 0; i < passengers.length; i++) {
-    if (testFunction(passengers[i])) {
-      return false;
-    }
-  }
-  return true;
-}
+// function processPassengers(passengers, testFunction) {
+//   for (let i = 0; i < passengers.length; i++) {
+//     if (testFunction(passengers[i])) {
+//       return false;
+//     }
+//   }
+//   return true;
+// }
 
-function checkNoFlyList(passenger) {
-  return passenger.name === "Dr. Evel";
-}
+// function checkNoFlyList(passenger) {
+//   return passenger.name === "Dr. Evel";
+// }
 
-function checkNotPaid(passenger) {
-  return !passenger.paid;
-}
+// function checkNotPaid(passenger) {
+//   return !passenger.paid;
+// }
 
+// function printPassenger(passenger) {
+//   console.log(
+//     passenger.paid
+//       ? `${passenger.name} has paid`
+//       : `${passenger.name} has not paid`
+//   );
+// }
 
+// function createDrinkOrder(passenger) {
+//   let orderFunction;
+//   if (passenger.ticket === "firstclass") {
+//     orderFunction = function () {
+//       alert("Would you like a cocktail or wine?");
+//     };
+//   } else {
+//     orderFunction = function () {
+//       alert("You choice is cola or water.");
+//     };
+//   }
+//   return orderFunction;
+// }
 
-function printPassenger(passenger) {
-  console.log(
-    passenger.paid
-      ? `${passenger.name} has paid`
-      : `${passenger.name} has not paid`
-  );
-}
+// function serveCustomer(passenger) {
+//   let getDrinkOrderFunction = createDrinkOrder(passenger);
+//   getDrinkOrderFunction();
+// }
 
-function createDrinkOrder(passenger) {
-  let orderFunction;
-  if (passenger.ticket === "firstclass") {
-    orderFunction = function () {
-      alert("Would you like a cocktail or wine?");
-    };
+// function servePassengers(passengers) {
+//   for (let i = 0; i < passengers.length; i++) {
+//     serveCustomer(passengers[i]);
+//   }
+// }
+
+// servePassengers(passengers);
+
+// let allCanFly = processPassengers(passengers, checkNoFlyList);
+// if (!allCanFly) {
+//   console.log(
+//     "The plane can't take off: we have a passenger on the no-fly-list."
+//   );
+// }
+
+// let allPaid = processPassengers(passengers, checkNotPaid);
+// if (!allPaid) {
+//   console.log("The plane can't take off: not everyone has paid.");
+// }
+
+// processPassengers(passengers, printPassenger);
+// --------------------------------------------------------
+
+let numberArray = [60, 50, 62, 58, 54, 54];
+
+numberArray.sort(compareNumbers)
+console.log(numberArray);
+
+function compareNumbers(num1, num2) {
+  if (num1 > num2) {
+    return 1;
+  } else if (num1 === num2) {
+    return 0;
   } else {
-    orderFunction = function () {
-      alert("You choice is cola or water.");
-    };
-  }
-  return orderFunction;
-}
-
-function serveCustomer(passenger) {
-  let getDrinkOrderFunction = createDrinkOrder(passenger);
-  getDrinkOrderFunction();
-}
-
-function servePassengers(passengers) {
-  for (let i = 0; i < passengers.length; i++) {
-    serveCustomer(passengers[i]);
+    return -1;
   }
 }
 
-servePassengers(passengers);
 
-let allCanFly = processPassengers(passengers, checkNoFlyList);
-if (!allCanFly) {
-  console.log(
-    "The plane can't take off: we have a passenger on the no-fly-list."
-  );
-}
-
-let allPaid = processPassengers(passengers, checkNotPaid);
-if (!allPaid) {
-  console.log("The plane can't take off: not everyone has paid.");
-}
-
-processPassengers(passengers, printPassenger);
