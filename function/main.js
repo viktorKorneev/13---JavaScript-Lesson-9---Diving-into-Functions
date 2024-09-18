96f8e75e338f9e4d20ad9674ff900657c86d661a
viktorKorneev <korneevviktor90@yandex.ru>
2024-09-18 22:31:48 +0300

    step-15

--- function/main.js
+++ function/main.js
@@ -123,19 +123,32 @@
 // processPassengers(passengers, printPassenger);
 // --------------------------------------------------------
 
-let numberArray = [60, 50, 62, 58, 54, 54];
+// let numberArray = [60, 50, 62, 58, 54, 54];
 
-numberArray.sort(compareNumbers)
-console.log(numberArray);
+// numberArray.sort(compareNumbers);
 
-function compareNumbers(num1, num2) {
-  if (num1 > num2) {
-    return 1;
-  } else if (num1 === num2) {
-    return 0;
-  } else {
-    return -1;
-  }
-}
+// console.log(numberArray);
+
+// function compareNumbers(num1, num2) {
+//   if (num1 > num2) {
+//     return 1;
+//   } else if (num1 === num2) {
+//     return 0;
+//   } else {
+//     return -1;
+//   }
+// }
+// -----------------------------------------------------------
+
+let products = [
+  { name: "Grapefruit", calories: 170, color: "red", sold: 8200 },
+  { name: "Orange", calories: 160, color: "orange", sold: 12101 },
+  { name: "Cola", calories: 210, color: "caramel", sold: 25412 },
+  { name: "Diet Cola", calories: 0, color: "caramel", sold: 43922 },
+  { name: "Lemon", calories: 200, color: "clear", sold: 14983 },
+  { name: "Raspberry", calories: 180, color: "pink", sold: 9427 },
+  { name: "Root Beer", calories: 200, color: "caramel", sold: 9909 },
+  { name: "Water", calories: 0, color: "clear", sold: 62123 },
+];
 
 
